58e50e68eee9b7f1f05781821655cdf8bb0b6f7f
N6mnguyen <namsun3113@gmail>
2024-10-22 15:18:07 +0700

    fix

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -5,18 +5,14 @@ import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
 import { RoomsComponent } from './components/rooms/rooms.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ViewBookingsComponent } from './components/view-bookings/view-bookings.component';
+import { ViewBookingComponent } from './components/view-booking/view-booking.component';
 
 
 @NgModule({
   declarations: [
     CustomerComponent,
     RoomsComponent,
-    ViewBookingsComponent
+    ViewBookingComponent
   ],
   imports: [
     CommonModule,
